24602eecd93e5f5eee6b590f0a841be7ee456ee1
NikCodePro <vermanikhil7838@gmail.com>
2024-07-05 15:24:50 +0530

    commit

--- Express js/handling-requests/main.js
+++ Express js/handling-requests/main.js
@@ -1,27 +1,21 @@
 import express from 'express'
 const app = express()
-const port = 5000
+const port = 3000
 
-app.use(express.static("public"))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-    console.log(" It's a get Request")
-})
-
-app.post('/', (req, res) => {
-    res.send('Hello post!')
-    console.log(" It's a post Request")
-})
-
-app.put('/', (req, res) => {
-    res.send('Hello put!')
-    console.log(" It's a put Request")
-})
-
-app.delete('/', (req, res) => {
-    res.send('Hello delete!')
-    console.log(" It's a delete Request")
+    console.log("It's a get Request")
+    res.send('Hello World! .......get Reqs')
+}).post('/', (req, res) => {
+    console.log("It's a post Request")
+    res.send('Hello World! .......POST Reqs')
+}).put('/', (req, res) => {
+    console.log("It's a put Request")
+    res.send('Hello World! .......put Reqs')
+}).delete('/', (req, res) => {
+    console.log("It's a delete Request")
+    res.send('Hello World! .......delete Reqs')
 })
 
 app.listen(port, () => {
