ddc1cd081a1b31e102675eeb0e0677df05d731c2
NikCodePro <vermanikhil7838@gmail.com>
2024-07-10 15:04:02 +0530

    mongoose

--- MongoDB/intro to Mongoose/main.js
+++ MongoDB/intro to Mongoose/main.js
@@ -1,3 +1,18 @@
-const mongoose = require('mongoose')
+import mongoose from "mongoose"
+import express from "express"
+import { Todo } from './models/todo.js'
 
-await mongoose.connect('mongodb://localhost:27017/')
+const connection = await mongoose.connect('mongodb://localhost:27017/')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    const todo = new Todo({ title: 2, desc: "this page is done by express or mongo db", idDone: false, days: 4 })
+    todo.save()
+    res.send("Hello Mongo DB")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
